5325893bc3e7afb41998f10d22b42a48eb88617c
Daniel-Wolski <20098120@mail.wit.ie>
2023-11-08 17:57:01 +0000

    Lab work copied

---
+++ movies-assignment/src/pages/upcomingMoviesPage.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { getUpcomingMovies } from "../api/tmdb-api";
+import PageTemplate from '../components/templateUpcomingMoviesList';
+import { useQuery } from 'react-query';
+import Spinner from '../components/spinner';
+import AddToWatchListIcon from '../components/cardIcons/addToWatchlList'
+
+const UpcomingMoviesPage = () => {
+
+ const { data, error, isLoading, isError } = useQuery('upcoming', getUpcomingMovies)
+ if (isLoading) {
+    return <Spinner />
+ }
+
+ if (isError) {
+    return <h1>{error.message}</h1>
+ }  
+ const upcomingMovies = data.results;
+
+ // Redundant, but necessary to avoid app crashing.
+ const upcomingList = upcomingMovies.filter(m => m.upcomingList)
+ localStorage.setItem('upcomingList', JSON.stringify(upcomingList))
+ const addToWatchList = (movieId) => true 
+
+ return (
+    <PageTemplate
+      upcomingMovies={upcomingMovies}
+      title="Upcoming Movies"
+      action={(movie) => {
+         return <AddToWatchListIcon movie={movie} />
+      }}
+    />
+);
+};
+export default UpcomingMoviesPage;
